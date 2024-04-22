e3f5772f20c6624605d9be958b5a8b77cf4a65f3
Tello M. Josue A. <josue.tello5730@alumno.udg.mx>
2024-04-21 18:56:37 -0600

    reduce files

--- src/main/java/com/telloing/frame/Chracters/ChracterBuilder/OrnamentDirector.java
+++ src/main/java/com/telloing/frame/Chracters/ChracterBuilder/OrnamentDirector.java
@@ -6,15 +6,74 @@ package com.telloing.frame.Chracters.ChracterBuilder;
 
 
 import com.telloing.frame.Chracters.ChracterBuilder.OrnamentBuilder;
-import com.telloing.frame.Chracters.MovCharact;
+import com.telloing.frame.Frames.Animations;
+import com.telloing.frame.Chracters.ChracterAttri;
 import com.telloing.frame.Chracters.Ornament;
 import java.awt.Container;
 
+import com.telloing.frame.Scenary;
+import java.awt.image.BufferedImage;
+import java.util.Hashtable;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
  * @author aleck
  */
+
+class OrnamentBuilder {
+    Ornament ornament;
+    Container container;
+    ChracterAttri attributos;
+    private static OrnamentBuilder ornamentBuilder;
+    Hashtable<String, Animations> listAnimations;
+    
+    private OrnamentBuilder() {
+        listAnimations = new Hashtable<String, Animations>();
+    }
+    
+    public static OrnamentBuilder getInstance() {
+        if (ornamentBuilder == null)
+            ornamentBuilder = new OrnamentBuilder();
+        return ornamentBuilder;
+    }
+
+    public void buildContainer(Container container) {
+        this.container = container;
+    }
+    
+    public void buildAttri(final int x, final int y){
+        this.attributos = new ChracterAttri(x, y, 1, this.listAnimations);
+    }
+
+    public Ornament getOrnament() {
+        // Define here the position of the ornament
+        this.ornament = new Ornament(this.attributos, this.container);
+        return this.ornament;
+    }
+    
+    public void buildFrames(String nameFile) {
+        BufferedImage setFrames;
+        
+        try {
+            setFrames = ImageIO.read(getClass().getResourceAsStream(nameFile));
+        } catch (IOException ex) {
+            Logger.getLogger(Scenary.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
+        
+        Animations anim;
+        
+        // Need to defien an image
+        anim = new Animations(Animations.separateFrames(setFrames, 66, 48, 1, 12));
+        listAnimations.clear();
+        listAnimations.put("Ondulamiento", anim);
+    }
+}
+
 public class OrnamentDirector {
     private OrnamentBuilder builder;
     private static OrnamentDirector ornamentDirector;
