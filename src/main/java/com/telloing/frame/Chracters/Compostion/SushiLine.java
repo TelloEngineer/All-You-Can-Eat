c21c04a694428b5550e7e6f7d9928690e917ba26
Tello M. Josue A. <josue.tello5730@alumno.udg.mx>
2024-04-26 21:57:24 -0600

    delete sushis correct

--- src/main/java/com/telloing/frame/Chracters/Compostion/SushiLine.java
+++ src/main/java/com/telloing/frame/Chracters/Compostion/SushiLine.java
@@ -12,139 +12,152 @@ import com.telloing.frame.Chracters.Food;
 
 import java.awt.Graphics2D;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  *
  * @author josue
  */
 
- import java.awt.Container;
- import java.awt.event.KeyEvent;
+import java.awt.Container;
+import java.awt.event.KeyEvent;
 import java.lang.reflect.Array;
 
- 
 class Sushi_Online implements ActCharac {
- 
-     private Food sushi;
-     private Container container;
- 
-     public Sushi_Online(Food sushi, Container container) {
-         this.sushi = sushi;
-         this.container = container;
-     }
- 
-     
-     public Container getContainer() {
-         return container;
-     }
- 
-     public void setContainer(Container container) {
-         this.container = container;
-     }
- 
- 
-     public Food getSushi() {
-         return sushi;
-     }
- 
-     public void setSushi(Food sushi) {
-         this.sushi = sushi;
-     }
- 
-     @Override
-     public void draw(Graphics2D g) {
-         g.drawImage(sushi.getAttributes().getImage(), sushi.getAttributes().getX(), sushi.getAttributes().getY(),
-                 container);
-     }
- 
-     @Override
-     public void update() {
-        sushi.getAttributes().setX(sushi.getAttributes().getX() + sushi.getAttributes().getSpeed());
-     }
- 
-     
- }
- 
 
-public class SushiLine implements ActCharac{
-    private List<Food> sushis;
-    private Sushi_Online action;
+    private Food sushi;
+    private Container container;
 
-    public SushiLine() {
-        this.sushis = new LinkedList<>();
-        this.action = new Sushi_Online(null, null);
+    public Sushi_Online(Food sushi, Container container) {
+        this.sushi = sushi;
+        this.container = container;
     }
 
-    public boolean add(Food character){
-        if(this.sushis.size() >= 32){
-            return false;
-        }
-        
-        action.setContainer(character.getContainer());
-        setNewCollisionerArea(430, 3, 2);
-        return this.sushis.add(character);
+    public Container getContainer() {
+        return container;
     }
-    public boolean remove(Food character){
-        return this.sushis.remove(character);
+
+    public void setContainer(Container container) {
+        this.container = container;
     }
-    
-    public Food getChild(Food character){
-        int index = this.sushis.indexOf(character);
-        if(index == -1){
-            return null;
-        }
-        return this.sushis.get(index);
+
+    public Food getSushi() {
+        return sushi;
     }
-    
+
+    public void setSushi(Food sushi) {
+        this.sushi = sushi;
+    }
+
     @Override
     public void draw(Graphics2D g) {
-      for(Food element : sushis){
-            action.setSushi(element);
-            action.draw(g);
-      }
+        g.drawImage(sushi.getAttributes().getImage(), sushi.getAttributes().getX(), sushi.getAttributes().getY(),
+                container);
     }
 
     @Override
     public void update() {
-        for(Food element : sushis){
-            action.setSushi(element);
-            checkListener(element);
-            System.out.println(Arrays.toString(CollisionerPlaneArea.collisionFood));
+        sushi.getAttributes().setX(sushi.getAttributes().getX() + sushi.getAttributes().getSpeed());
+    }
+
+}
+
+public class SushiLine implements ActCharac {
+    private List<Food> sushis;
+    private List<Food> sushisToRemove;
+    private ListIterator<Food> iterador;
+    private Sushi_Online action;
+
+    public SushiLine() {
+        this.sushis = new LinkedList<>();
+        this.sushisToRemove = new LinkedList<>();
+        this.iterador = sushis.listIterator();
+        this.action = new Sushi_Online(null, null);
+    }
+
+    public boolean add(Food character) {
+        if (this.sushis.size() >= 32) {
+            return false;
+        }
+
+        action.setContainer(character.getContainer());
+        setNewCollisionerArea(430, 3, 2);
+        return this.sushis.add(character);
+    }
+
+    public boolean remove(Food character) {
+        return this.sushis.remove(character);
+    }
+
+    public Food getChild(Food character) {
+        int index = this.sushis.indexOf(character);
+        if (index == -1) {
+            return null;
+        }
+        return this.sushis.get(index);
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        Food sushi;
+        for(iterador = sushis.listIterator(); iterador.hasNext();) { //se ocupa actualizar cada vez que se itera
+            sushi = iterador.next(); //sirve como una pila, asi que lo saca de la misma
+
+            action.setSushi(sushi);
+            action.draw(g);
         }
     }
 
-    private void checkListener(Food sushi){
+    @Override
+    public void update() {
+        Food sushi;
+        sushisToRemove.clear(); //lista para guardar cuales a eliminar
+        for(iterador = sushis.listIterator(); iterador.hasNext();) {//se ocupa actualizar cada vez que se itera
+            sushi = iterador.next(); //sirve como una pila, asi que lo saca de la misma
+
+            action.setSushi(sushi);
+            checkListener(sushi);
+            checkCollision(sushi); // default: action.update();
+        }
+        sushis.removeAll(sushisToRemove); // revisa los sushis a eliminar, A HUEVO, se hace asi
+        // porque si borramos directamente, y nos quedamos sin sushis, EXCEPTION  java.util.ConcurrentModificationException
+    }
+
+    private void checkListener(Food sushi) {
         switch (Scenary.listener.getKeyCode()) {
             case KeyEvent.VK_E:
-                setNewCollisionerArea(420, 2, 3);
+                setNewCollisionerArea(420, 5, 3);
                 break;
             default:
-                setNewCollisionerArea(420, 2, 0);
+                setNewCollisionerArea(420, 5, 0);
         }
         Scenary.listener.setKeyCode(-1);
-        this.checkCollision(sushi.getCollisionChecker().updateCollision(sushi.getAttributes().getSpeed()), sushi);
+
     }
 
-    private void checkCollision(int idObj, Food sushi){
+    private void checkCollision(Food sushi) {
+        int idObj = sushi.getCollisionChecker().updateCollision(sushi.getAttributes().getSpeed());
         switch (idObj) {
             case 2:
-            sushi.getAttributes().setX(72);
-            sushi.getCollisionChecker().getCollisionZone()[sushi.getCollisionChecker().getActualPosition()] = 0;
-            sushi.getCollisionChecker().setActualPosition(0);
+                sushi.getAttributes().setX(72);
+                sushi.getCollisionChecker().getCollisionZone()[sushi.getCollisionChecker().getActualPosition()] = 0;
+                sushi.getCollisionChecker().setActualPosition(0);
                 break;
             case 3:
-            sushi.getCollisionChecker().getCollisionZone()[sushi.getCollisionChecker().getActualPosition()] = 0;
-            sushi.getCollisionChecker().setActualPosition(0);
-            sushis.remove(sushi);
+                sushi.getCollisionChecker().getCollisionZone()[sushi.getCollisionChecker().getActualPosition()] = 0;
+                sushi.getCollisionChecker().setActualPosition(0);
+                System.out.println(sushisToRemove.add(sushi));
                 break;
             default:
                 action.update();
         }
     }
-    private void setNewCollisionerArea(int point, int dimension, int symbol){
-        for(int i = 0; i < dimension; i++){
+
+    private void setNewCollisionerArea(int point, int dimension, int symbol) {
+        for (int i = 0; i < dimension; i++) {
             CollisionerPlaneArea.collisionFood[point + i] = symbol;
         }
     }
--- src/main/java/com/telloing/frame/Scenary.java
+++ src/main/java/com/telloing/frame/Scenary.java
@@ -38,7 +38,7 @@ public class Scenary extends JPanel implements Runnable {
         drawInterval = 1000 / fps;
 
         listener = new MovCharact();
-        for(int i = 0; i<1; i++){
+        for(int i = 0; i<3; i++){
             System.out.print(i);
             System.out.println(Scenary.sushis.add(FoodDirector.getInstancia().createSushi1(this, listener)));
         }
